c1afa49215a7615a13a22032fc93eb8bae528b28
unknown <nickgafron@gmail.com>
2020-04-16 18:31:39 -0500

    basic map

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoibmdhZnJvbiIsImEiOiJjazd3azI2NTEwM2Q3M2RxdTNxa3o0YzM3In0.PLONfIKTEAgoFlVGqZVRiQ";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,11 @@
-function createMap(bikeStations) {
+function createMap(quakeLocations) {
 
     // Create the tile layer that will be the background of our map
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
         maxZoom: 18,
         id: "mapbox.light",
-        accessToken: API_KEY
+        accessToken: "pk.eyJ1IjoibmdhZnJvbiIsImEiOiJjazd3azI2NTEwM2Q3M2RxdTNxa3o0YzM3In0.PLONfIKTEAgoFlVGqZVRiQ"
     });
 
     // Create a baseMaps object to hold the lightmap layer
@@ -15,14 +15,14 @@ function createMap(bikeStations) {
 
     // Create an overlayMaps object to hold the bikeStations layer
     var overlayMaps = {
-        "Bike Stations": bikeStations
+        "Quakes": quakeLocations
     };
 
     // Create the map object with options
     var map = L.map("map-id", {
         center: [40.73, -74.0059],
-        zoom: 12,
-        layers: [lightmap, bikeStations]
+        zoom: 3,
+        layers: [lightmap, quakeLocations]
     });
 
     // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
@@ -31,38 +31,41 @@ function createMap(bikeStations) {
     }).addTo(map);
 }
 
-function createMarkers(response) {
+function createCircles(response) {
 
     // Pull the "stations" property off of response.data
-    var stations = response.data.stations;
+    var quakes = response.features;
+    console.log(quakes);
+    // var quake = quakes[0].geometry.coordinates;
+    // console.log(quake[0])
 
     // Initialize an array to hold bike markers
-    var Markers = [];
+    var Circles = [];
 
     // Loop through the stations array
-    for (var index = 0; index < stations.length; index++) {
-        var station = stations[index];
+    for (var i = 0; i < quakes.length; i++) {
+        var quake =  quakes[i].geometry.coordinates;
 
         // For each station, create a marker and bind a popup with the station's name
-        var Marker = L.marker([station.lat, station.lon])
-            .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+        var Circle = L.marker([quake[1], quake[0]])
+            .bindPopup("<h3>" + quakes[i].properties.place + "<h3><h3>Magnitude: " + quakes[i].properties.mag + "</h3>");
 
-        // Add the marker to the bikeMarkers array
-        Markers.push(Marker);
+        // Add the marker to the Circles array
+        Circles.push(Circle);
     }
 
     // Create a layer group made from the bike markers array, pass it into the createMap function
-    createMap(L.layerGroup(bikeMarkers));
+    createMap(L.layerGroup(Circles));
 }
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson", createCircles);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson", createCircles);
 
 //-----------------------------------------------------------//
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+// var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(mymap);
